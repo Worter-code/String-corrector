191fa57d3050c366a91a82d3af901cf724db1560
Worter-code <nenashevvanya1@gmail.com>
2023-05-19 23:10:29 +0300

    Func for checking name

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import re
 
+
+EMPTY_TEXT = ' - '
+
+
+def correct_name(query):
+    corrected = re.sub(r'[^а-яА-Я]+', '', query)
+    if len(corrected) > 0:
+        return corrected.capitalize()
+    else:
+        return EMPTY_TEXT
+
+
+if __name__ == '__main__':
+    print('test')
